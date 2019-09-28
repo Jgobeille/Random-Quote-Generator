faabbff8f14cda9fde7139455cc0acfafcdc5c64
Jamie Gobeille <djent34@gmail.com>
2019-09-28 01:26:38 -0400

    Added print quote function & add audio

--- js/script.js
+++ js/script.js
@@ -20,96 +20,55 @@ let quotes = [
   {
     quote: "Pride is not the opposite of shame, but its source.",
     source: "Uncle Iroh",
-    citation: "Episode 2.09 'Bitter Work'",
-    year: 1985
-
+    citation: "Episode 2.09 'Bitter Work'"
   },
   {
     quote: "Sharing tea with a fascinating stranger is one of life’s true delights.",
     source: "Uncle Iroh",
-    citation: "Episode 2.08 'The Chase'",
-    year: 1985
-
+    citation: "Episode 2.08 'The Chase'"
   },
   {
     quote: "Sometimes life is like this tunnel. You can’t always see the light at the end of the tunnel, but if you keep moving, you will come to a better place.",
     source: "Uncle Iroh",
-    citation: "Episode 2.19 'The Crossroads of Destiny'",
-    year: 1985
-
+    citation: "Episode 2.19 'The Crossroads of Destiny'"
   },
   {
     quote: "Life happens wherever you are, whether you make it or not.",
     source: "Uncle Iroh",
-    citation: "Episode 2.13 'City of Walls Secrets'",
-    year: 1985
-
+    citation: "Episode 2.13 'City of Walls Secrets'"
   },
   {
     quote: "You have come to the crossroads of your destiny. It is time for you to choose. It is time for you to choose good.",
     source: "Uncle Iroh",
-    citation: "Episode 2.20 'The Crossroads of Destiny'",
-    year: 1985
-
+    citation: "Episode 2.20 'The Crossroads of Destiny'"
   },
   {
     quote: "There is nothing wrong with a life of peace and prosperity. I suggest you think about what it is you want from your life, and why.",
     source: "Uncle Iroh",
-    citation: "Episode 2.17 'Lake Laogai'",
-    year: 1985
-
+    citation: "Episode 2.17 'Lake Laogai'"
   },
   {
     quote: "IT IS IMPORTANT TO DRAW WISDOM FROM MANY DIFFERENT PLACES",
     source: "Uncle Iroh",
-    citation: "Episode 2.09 'Bitter Work'",
-    year: 1985
-
+    citation: "Episode 2.09 'Bitter Work'"
   },
   {
     quote: "HOPE IS SOMETHING YOU GIVE YOURSELF. THAT IS THE MEANING OF INNER STRENGTH.",
     source: "Uncle Iroh",
-    citation: "ddsfad",
-    year: 1985
-
   },
   {
     quote: "DESTINY IS A FUNNY THING. YOU NEVER KNOW HOW THINGS ARE GOING TO WORK OUT.",
     source: "Uncle Iroh",
-    citation: "ddsfad",
-    year: 1985
-
   },
   {
     quote: "WHILE IT IS ALWAYS BEST TO BELIEVE IN ONESELF, A LITTLE HELP FROM OTHERS CAN BE A GREAT BLESSING.",
     source: "Uncle Iroh",
-    citation: "ddsfad",
-    year: 1985
-
   },
   {
     quote: "IT IS USUALLY BEST TO ADMIT MISTAKES WHEN THEY OCCUR, AND TO SEEK TO RESTORE HONOR.",
     source: "Uncle Iroh",
-    citation: "ddsfad",
-    year: 1985
-
   },
 ]
-
-
-function getRandomQuote() {
-const max = quotes.length;
-
-let randomNumber = Math.floor(Math.random() * max - 1 + 1) + 1; 
-
-return quotes[randomNumber].quote;
-}
-
-
-console.log(getRandomQuote());
-
-
-
 /***
   Create the `getRandomQuote` function to:
    - Create a variable to store a random number 
@@ -117,6 +76,52 @@ console.log(getRandomQuote());
 ***/
 
 
+/*
+This will make a random number between one 
+and the max number of items in the array. 
+
+Next it will return the quotes array and access 
+any object in the array with the random number
+*/
+function getRandomQuote() {
+const max = quotes.length;
+
+let randomNumber = Math.floor(Math.random() * max - 1 + 1); 
+console.log(randomNumber);
+let quote = quotes[randomNumber];
+return quotes[randomNumber];
+}
+
+
+
+//Create function that prints the quote to the page. Good idea is to set each section of the object to a var and then append the text to the HTML already present on the page. ...Maybe
+function printQuote() {
+let randomQuote = getRandomQuote();
+let quoteText = '';
+let HTML = document.getElementById('quote-box');
+let quote = randomQuote.quote;
+let upperCase = quote.toUpperCase();
+
+
+quoteText += '<p class="quote">' + upperCase + '</p>';
+quoteText += '<p class="source">' + randomQuote.source;
+if(randomQuote.citation) {
+  quoteText += '<span class="citation">' + randomQuote.citation + '</span>';
+} 
+if(randomQuote.year) {
+  quoteText += '<span class="year">' + randomQuote.year + '</span>';
+}
+quoteText += '</p>';
+HTML.innerHTML = quoteText;
+
+}
+
+
+
+
+window.setInterval(printQuote, 5000);
+
+printQuote();
 
 
 /***
