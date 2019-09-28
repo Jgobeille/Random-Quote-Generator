95fa641e60e55e515eca8354a62add72ccfbc929
Jamie Gobeille <djent34@gmail.com>
2019-09-28 12:48:33 -0400

    added notes to javascript file

--- js/script.js
+++ js/script.js
@@ -6,14 +6,7 @@ project 1 - A Random Quote Generator
 // Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
 
 
-/*** 
-  Create the array of quote objects and name it `quotes`.
-  Add at least five quote objects to the `quotes` array.
-  Give each quote object a `quote` and `source` property.
-  Add the `citation` property to at least one object in the array.
-  Add the `year` property to at least one object in the array.
-  Use console.log() to log your array of quotes to the console.
-***/
+
 
 // Array of objects that will hold quote information
 let timer = window.setInterval(printQuote, 10000);
@@ -70,11 +63,7 @@ let quotes = [
     source: "Uncle Iroh",
   },
 ]
-/***
-  Create the `getRandomQuote` function to:
-   - Create a variable to store a random number 
-   - Cse the random number to `return` a random quote object from the `quotes` array.
-***/
+
 
 
 /*
@@ -95,6 +84,7 @@ return quotes[randomNumber];
 
 
 
+
 //Create function that prints the quote to the page. Good idea is to set each section of the object to a var and then append the text to the HTML already present on the page. ...Maybe
 function printQuote() {
 let randomQuote = getRandomQuote();
@@ -103,20 +93,24 @@ let HTML = document.getElementById('quote-box');
 let quote = randomQuote.quote;
 let upperCase = quote.toUpperCase();
 
-
+//compile all text from quotes object into to quote text var
 quoteText += '<p class="quote">' + upperCase + '</p>';
 quoteText += '<p class="source">' + randomQuote.source;
+//check if citation and year are included
 if(randomQuote.citation) {
   quoteText += '<span class="citation">' + randomQuote.citation + '</span>';
 } 
 if(randomQuote.year) {
   quoteText += '<span class="year">' + randomQuote.year + '</span>';
 }
+//close paragraph tag and print to the page
 quoteText += '</p>';
 HTML.innerHTML = quoteText;
 
+//clear timer interval when button is pressed and then reset the timer again
 window.clearInterval(timer);
 timer = window.setInterval(printQuote, 10000);
+//run randomNumber function
 randomColor(255, 1);
 
 }
@@ -124,46 +118,30 @@ randomColor(255, 1);
 //randomly changes the color of the button each time a new quote appears
 function randomColor(upper, lower) {
   //set color vars to random numbers between 1 and 255)
+    lower = Math.ceil(lower);
+    upper = Math.floor(upper);
     let red = Math.floor(Math.random() * upper - lower + lower) + lower;
     let blue = Math.floor(Math.random() * upper - lower + lower) + lower;
     let green = Math.floor(Math.random() * upper - lower + lower) + lower;
     let bgColor = `rgb(${red},${blue},${green})`;
-    document.getElementById("loadQuote").style.backgroundColor = bgColor;
+    let button = document.getElementById("loadQuote");
+    button.style.backgroundColor = bgColor;
 }
 
 
-
-//timer rest if click function
-const click = document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-
+//when button is clicked, run printQuote function
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
 
 
 
 
-/***
-  Create the `printQuote` function to: 
-   - Call the `getRandomQuote` function and assign it to a variable.
-   - Create a variable for the HTML string and set it equal to an empty string.
-   - Use the HTML template in the instructions or the markup in the index.html file, AND 
-     the random quote vairable to build your HTML string.
-   - Add the quote and source section to the HTML string.
-   - Use an if statement to check for the citation property before adding it to the HTML string.
-   - Use an if statement to check for the year property before adding it to the HTML string.
-   - Don't forget to close that final `p` tag.
-   - Set the `innerHTML` of the `quote-box` div to the HTML string. 
-***/
 
 
 
 
 
-/***
-  When the "Show another quote" button is clicked, the event listener 
-  below will be triggered, and it will call, or "invoke", the `printQuote` 
-  function. So do not make any changes to the line of code below this 
-  comment.
-***/
 
 
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+
+
